bdc2d95e99b6f2901b569a39d0a2d0a548a9c896
klapeyron5 <nikitaklapeyron@gmail.com>
2023-04-12 07:25:55 +0000

    L05_HW exception test

--- hw_tests_prod/test_L05_HW_project.py
+++ hw_tests_prod/test_L05_HW_project.py
@@ -20,17 +20,28 @@ def test_func(**kwargs):
     stdout = kwargs[test.OUTPUT_stdout]
     rtrn = kwargs[test.OUTPUT_returned]
 
-    assert rtrn is None
-
-    assert stdout[:4] == 'SE: '
-    se = int(stdout[4:])
-    e = 1+1*int(stdin[0]) - int(stdin[1])
-    assert e**2 == se
+    if rtrn is None:
+        assert stdout[:4] == 'SE: '
+        se = int(stdout[4:])
+        a0 = int(stdin[0])
+        a1 = int(stdin[1])
+        e = 1+1*a0 - a1
+        assert e**2 == se
+    elif isinstance(rtrn, ValueError):
+        try:
+            a0 = int(stdin[0])
+            a1 = int(stdin[1])
+            raise Exception
+        except ValueError as e:
+            assert isinstance(rtrn,ValueError)
+    else:
+        raise Exception
 
 
 cases = [{test.INPUT_stdin: x, test.TEST_FUNC: test_func} for x in [
     [1, 3],
     [10, '2'],
+    [10,'!'],
 ]]
 
 
